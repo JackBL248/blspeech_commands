df8222cbe34f38a0225999263eb8a70cebc47ae6
JackBL <jbleveson@gmail.com>
2020-09-22 14:05:12 +0100

    created model class

--- model.py
+++ model.py
@@ -2,89 +2,124 @@ import copy
 import time
 
 import torch
+import torch.nn as nn
+from torchvision import models
+
+from config import Config
 
 
-def train_model(model, criterion, optimizer, num_epochs=25):
-    since = time.time()
+class Model(object):
 
-    best_model_wts = copy.deepcopy(model.state_dict())
-    best_acc = 0.0
-    best_loss = 1000.00
-    # Set counter for early stopping
-    early_stop_counter = 0
+    def __init__(self, model_type):
+        self.device = torch.device(
+            "cuda:0" if torch.cuda.is_available() else "cpu"
+            )
+        if model_type == "alexnet":
+            self.model = models.alexnet(pretrained=True)
+            self.model.classifier[6] = nn.Linear(4096, Config.NUM_CLASSES)
+            self.model = self.model.to(self.device)
 
-    for epoch in range(num_epochs):
-        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
-        print('-' * 10)
+    def train_model(
+        self,
+        model,
+        criterion,
+        optimizer,
+        dataloaders,
+        dataset_sizes,
+        num_epochs=25,
+        patience=15,
+    ):
+        """
+        Trains a neural network with early stopping.
+        -----------------------
+        Args: PyTorch model, training criterion, optimizer, num epochs
+        and early stopping patience
 
-        # Each epoch has a training and validation phase
-        for phase in ['train', 'test']:
-            if phase == 'train':
-                model.train()  # Set model to training mode
-            else:
-                model.eval()   # Set model to evaluate mode
+        Returns: trained model with the best weights saved
+        -----------------------
+        """
+        since = time.time()
 
-            running_loss = 0.0
-            running_corrects = 0
+        best_model_wts = copy.deepcopy(model.state_dict())
+        best_acc = 0.0
+        best_loss = 1000.00
+        # Set counter for early stopping
+        early_stop_counter = 0
 
-            # Iterate over data.
-            for inputs, labels in dataloaders[phase]:
-                inputs = inputs.to(device)
-                labels = labels.to(device)
+        for epoch in range(num_epochs):
+            print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+            print('-' * 10)
 
-                # zero the parameter gradients
-                optimizer.zero_grad()
+            # Each epoch has a training and validation phase
+            for phase in ['train', 'val']:
+                if phase == 'train':
+                    model.train()  # Set model to training mode
+                else:
+                    model.eval()   # Set model to evaluate mode
 
-                # forward
-                # track history if only in train
-                with torch.set_grad_enabled(phase == 'train'):
-                    outputs = model(inputs)
-                    _, preds = torch.max(outputs, 1)
-                    loss = criterion(outputs, labels)
+                running_loss = 0.0
+                running_corrects = 0
 
-                    # backward + optimize only if in training phase
-                    if phase == 'train':
-                        loss.backward()
-                        optimizer.step()
+                # Iterate over data.
+                for inputs, labels in dataloaders[phase]:
+                    inputs = inputs.to(self.device)
+                    labels = labels.to(self.device)
 
-                # statistics
-                running_loss += loss.item() * inputs.size(0)
-                running_corrects += torch.sum(preds == labels.data)
+                    # zero the parameter gradients
+                    optimizer.zero_grad()
 
-            epoch_loss = running_loss / dataset_sizes[phase]
-            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+                    # forward
+                    # track history if only in train
+                    with torch.set_grad_enabled(phase == 'train'):
+                        outputs = model(inputs)
+                        _, preds = torch.max(outputs, 1)
+                        loss = criterion(outputs, labels)
 
-            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
-                phase, epoch_loss, epoch_acc))
+                        # backward + optimize only if in training phase
+                        if phase == 'train':
+                            loss.backward()
+                            optimizer.step()
 
-            # deep copy the model
-            if phase == 'test':
-                if epoch_acc > best_acc:
-                    best_acc = epoch_acc
-                    best_model_wts = copy.deepcopy(model.state_dict())
+                    # statistics
+                    running_loss += loss.item() * inputs.size(0)
+                    running_corrects += torch.sum(preds == labels.data)
 
-                if epoch_loss < best_loss:
-                    best_loss = epoch_loss
-                    # Reset early stopping counter to zero if there is
-                    # improvement in loss
-                    early_stop_counter = 0
+                epoch_loss = running_loss / dataset_sizes[phase]
+                epoch_acc = running_corrects.double() / dataset_sizes[phase]
 
-                elif epoch_loss >= best_loss:
-                    # Increment early stopping counter if there is no
-                    # improvement in loss
-                    early_stop_counter += 1
+                print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                    phase, epoch_loss, epoch_acc))
 
-            # If loss has not improved for 15 epochs, stop the training
-        if early_stop_counter == 15:
-            break
+                # deep copy the model
+                if phase == 'val':
+                    if epoch_acc > best_acc:
+                        best_acc = epoch_acc
+                        best_model_wts = copy.deepcopy(model.state_dict())
 
-        print()
+                    if epoch_loss < best_loss:
+                        best_loss = epoch_loss
+                        # Reset early stopping counter to zero if there is
+                        # improvement in loss
+                        early_stop_counter = 0
 
-    time_elapsed = time.time() - since
-    print('Training complete in {:.0f}m {:.0f}s'.format(
-        time_elapsed // 60, time_elapsed % 60))
-    print('Best test Acc: {:4f}'.format(best_acc))
+                    elif epoch_loss >= best_loss:
+                        # Increment early stopping counter if there is no
+                        # improvement in loss
+                        early_stop_counter += 1
 
-    # load best model weights
-    model.load_state_dict(best_model_wts)
-    return model
+                # If loss has not improved for 15 epochs, stop the training
+            if early_stop_counter == patience:
+                break
+
+            print()
+
+        time_elapsed = time.time() - since
+        print('Training complete in {:.0f}m {:.0f}s'.format(
+            time_elapsed // 60, time_elapsed % 60))
+        print('Best val Acc: {:4f}'.format(best_acc))
+
+        # load best model weights
+        model.load_state_dict(best_model_wts)
+        return model
+
+    def test_model()
