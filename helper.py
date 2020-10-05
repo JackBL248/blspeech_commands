d4afac1293dbe8dad27f495d3db4e7003fbaa7ce
JackBL <jbleveson@gmail.com>
2020-10-05 19:21:33 +0100

    add get_class_from_filename func

--- helper.py
+++ helper.py
@@ -1,10 +1,12 @@
 import numpy as np
 from skimage.transform import resize
 
+from config import Config
+
 
 def resize_preds(preds_list, x=224, y=224):
     '''
-    Function resizing all predictors to 224,224 for input to resnet
+    Resizes all predictors to 224,224 for input to model
     -----------------------
     Input: List of predictors, new x-dimension size, new y-dimension size
 
@@ -13,7 +15,23 @@ def resize_preds(preds_list, x=224, y=224):
     '''
     new_preds_list = []
     for preds in preds_list:
-        new_preds = resize(preds, (224, 224))
+        new_preds = resize(preds, (x, y))
         new_preds_list.append(new_preds)
 
     return np.array(new_preds_list)
+
+
+def get_class_from_filename(filename):
+    '''
+    Extracts class label from filename
+    -----------------------
+    Input: filename
+
+    Output: Class label
+    -----------------------
+    '''
+    # take the str class name from the filename
+    str_label = filename.split("_")[0]
+    # get the integer label
+    label = Config.CLASSES_DICT[str_label]
+    return label
