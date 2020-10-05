e804c87d02c934fd8513476baec367ff37c1ee23
JackBL <jbleveson@gmail.com>
2020-10-05 20:03:50 +0100

    remove metavars

--- args.py
+++ args.py
@@ -15,12 +15,14 @@ parser.add_argument('--model-arch', default="resnet18", type=str,
                     help='Type of CNN architecture')
 
 # ========================= Learning Configs ==========================
-parser.add_argument('--epochs', default=50, type=int, metavar='N',
+parser.add_argument('--epochs', default=50, type=int,
+                    help='number of total epochs to run')
+parser.add_argument('--epochs', default=50, type=int,
                     help='number of total epochs to run')
 parser.add_argument('-b', '--batch-size', default=16, type=int,
-                    metavar='N', help='mini-batch size (default: 16)')
+                    help='mini-batch size (default: 16)')
 parser.add_argument('--lr', '--learning-rate', default=0.001, type=float,
-                    metavar='LR', help='initial learning rate')
+                    help='initial learning rate')
 
 # ========================= Log Configs ==========================
 parser.add_argument('--log-file', default="log.txt", type=str,
