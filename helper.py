7aa6d92296e32d8d08baa0347397bf5608411b73
JackBL <jbleveson@gmail.com>
2020-10-08 14:06:48 +0100

    removed resized_preds func as no longer needed

--- helper.py
+++ helper.py
@@ -4,23 +4,6 @@ from skimage.transform import resize
 from config import Config
 
 
-def resize_preds(preds_list, x=224, y=224):
-    '''
-    Resizes all predictors to 224,224 for input to model
-    -----------------------
-    Input: List of predictors, new x-dimension size, new y-dimension size
-
-    Output: List of resized predictors
-    -----------------------
-    '''
-    new_preds_list = []
-    for preds in preds_list:
-        new_preds = resize(preds, (x, y))
-        new_preds_list.append(new_preds)
-
-    return np.array(new_preds_list)
-
-
 def get_class_from_filename(filename):
     '''
     Extracts class label from filename
