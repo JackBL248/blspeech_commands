c599dd38a0281ac94815c0e3c68daba3991c77ef
JackBL <jbleveson@gmail.com>
2020-10-08 17:13:23 +0100

    added expand_dims_twice func

--- helper.py
+++ helper.py
@@ -25,6 +25,11 @@ def get_class_from_filename(filename):
     return label
 
 
+def expand_dims_twice(array):
+    """convert array of shape (n,) to shape (n,1,1)"""
+    return np.expand_dims(np.expand_dims(array, axis=1), axis=1)
+
+
 def get_normalise_coefficients(train_preds):
     '''
     Finds the mean and std for channels from th
@@ -34,6 +39,9 @@ def get_normalise_coefficients(train_preds):
     Returns: mean and std for each channel
     -----------------------
     '''
-    training_mean = np.mean(train_preds, axis=(0, 2, 3))
-    training_std = np.std(train_preds, axis=(0, 2, 3))
-    return training_mean, training_std
+    training_means = np.mean(train_preds, axis=(0, 2, 3))
+    training_stds = np.std(train_preds, axis=(0, 2, 3))
+    # convert means and stds from shape (3,) to shape (3,1,1)
+    training_means = expand_dims_twice(training_means)
+    training_stds = expand_dims_twice(training_stds)
+    return training_means, training_stds
