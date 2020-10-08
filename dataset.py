853f9fb83870de7cb6a9c939b37d45eeff7f3367
JackBL <jbleveson@gmail.com>
2020-10-08 13:54:47 +0100

    created spectrogramDataset class

---
+++ dataset.py
@@ -0,0 +1,19 @@
+import torch
+import torch.utils.data as data
+
+
+class spectrogramDataset(data.Dataset):
+    def __init__(self, preds, labels, transform):
+        self.preds = preds
+        self.labels = labels
+        self.transform = transform
+
+    def __getitem__(self, index):
+        # prepare spectrogram for input into model
+        spectrogram = self.transform(self.preds[index])
+        # convert label to torch.Tensor.long type
+        label = torch.Tensor.long(torch.Tensor(self.labels[index]))
+        return spectrogram, label
+
+    def __len__(self):
+        return len(self.labels)
