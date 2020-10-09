5f54221757646e2c43386035f38f8f1f991b559b
JackBL <jbleveson@gmail.com>
2020-10-09 13:39:36 +0100

    corrected view_spec_from_file func

--- visualization.py
+++ visualization.py
@@ -5,14 +5,13 @@ from config import Config
 from preproc import preprocess_datapoint
 
 
-def view_spec(spec, label):
+def view_spec(spec):
     '''
     Views spectrogram using matplotlib.
     -----------------------
     Input: Spectrogram (np.array)
     -----------------------
     '''
-    str_label = Config.CLASSES_DICT_INVERTED[label]
     spec_copy = spec.transpose(2, 1, 0)
     spec_copy = resize(spec, (224, 224))
     spec_copy = rotate(spec_copy, 90)
@@ -33,5 +32,7 @@ def view_spec_from_file(wavfile):
     '''
     # Process data
     spec, label = preprocess_datapoint(wavfile)
+    str_label = Config.CLASSES_DICT_INVERTED[label]
+
     # view spectrogram
     view_spec(spec, label)
